3956c01c3f165c5f346a830bd7b5f8819f1c6f6d
Shaxmir <sharemixmir@gmail.com>
2019-02-11 16:03:48 +0300

    Задача решить

--- js/main.js
+++ js/main.js
@@ -16,3 +16,32 @@ iks.addEventListener('click', e => {
     } 
 });
 
+const accordion = document.querySelector('.accordion_item');
+const touch = document.querySelector('.accordion_link');
+
+
+touch.addEventListener('click', a => {
+    if(accordion.classList.contains('active') == false) {
+        accordion.classList.add('active');
+  
+
+    }
+    if(accordion.classList.contains('active') == true) {
+        accordion.classList.remove('active');
+    }
+});
+
+const acc = document.querySelector('.accord_item');
+const tou = document.querySelector('.accord_link');
+
+
+tou.addEventListener('click', a => {
+    if(acc.classList.contains('active') == false) {
+        acc.classList.add('active');
+  
+
+    }
+    if(acc.classList.contains('active') == true) {
+        acc.classList.remove('active');
+    }
+});
