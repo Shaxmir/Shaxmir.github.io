532b4d0cc2f5c07a31d348b4c1ddc9ab328387f1
Shaxmir <sharemixmir@gmail.com>
2019-02-19 21:06:33 +0300

    Обновление слайдера 10.02

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,30 @@
 $(document).ready(() => {
-    $('.scroll_left').on('click', function(e){
+    var totalWidth = 0;
+    $('.scroll_right').on('click', function(e){
+        var slideWidth = $('.slide_kadr').width();
+        var maxwidth = slideWidth * 5;
+        totalWidth = totalWidth + slideWidth;
         
-        var sldr = $('.slide_kadr'),
-        sldrContent = sldr.html(),
-        slideWidth = $('.slide_kadr').outerWidth(),
-        slideCount = $('.slide_kadr .slide_item_kadr').length - 1;
-        console.log(sldr);
-        console.log(sldrContent);
-        console.log(slideWidth);
-        console.log(slideCount);
-        $('.slide_kadr').css('right', slideWidth);
+        console.log(maxwidth);
+        if (totalWidth >= maxwidth) {
+            totalWidth = 0;
+            $('.slide_kadr').css('right',  totalWidth);
+        }else {
+            $('.slide_kadr').css('right', totalWidth);
+        }
     })
-});
+    $('.scroll_left').on('click', function(e){
+        var slideWidth = $('.slide_kadr').width();
+        var maxwidth = slideWidth * 4;
+        var minwidth = 0;
+        totalWidth = totalWidth - slideWidth;
+        
+        console.log(maxwidth);
+        if (totalWidth < minwidth) {
+            totalWidth = maxwidth;
+            $('.slide_kadr').css('right',  totalWidth);
+        }else {
+            $('.slide_kadr').css('right', totalWidth);
+        }
+    })
+ });
