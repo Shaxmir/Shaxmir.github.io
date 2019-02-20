2b067b4f94150e22aa79dbeb1aaaa0ef70649c64
Shaxmir <sharemixmir@gmail.com>
2019-02-20 20:05:00 +0300

    Обнова

--- js/jquery.js
+++ js/jquery.js
@@ -27,4 +27,28 @@ $(document).ready(() => {
             $('.slide_kadr').css('right', totalWidth);
         }
     })
+
+
+    $("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        
+        
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+
+
+    $('.nav_touch').on('click', function (params) {
+        $('.activ_nav').removeClass('activ_nav');
+		$(this).addClass('activ_nav');
+    });
+    
  });
