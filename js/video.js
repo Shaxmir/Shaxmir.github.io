a5efa90ad3bb146dc5a5112e0bf66060507b1615
Shaxmir <sharemixmir@gmail.com>
2019-02-23 21:40:01 +0300

    Плеер

--- js/video.js
+++ js/video.js
@@ -1,68 +1,137 @@
-var vid, playbtn, seekslider, curtimetext, durtimetext, mutebtn, volumeslider, fullscreenbtn;
-function intializePlayer(){
-	// Set object references
-	vid = document.getElementById("my_video");
-	playbtn = document.getElementById("playpausebtn");
-	seekslider = document.getElementById("seekslider");
-	curtimetext = document.getElementById("curtimetext");
-	durtimetext = document.getElementById("durtimetext");
-	mutebtn = document.getElementById("mutebtn");
-	volumeslider = document.getElementById("volumeslider");
-	fullscreenbtn = document.getElementById("fullscreenbtn");
-	// Add event listeners
-	playbtn.addEventListener("click",playPause,false);
-	seekslider.addEventListener("change",vidSeek,false);
-	vid.addEventListener("timeupdate",seektimeupdate,false);
-	mutebtn.addEventListener("click",vidmute,false);
-	volumeslider.addEventListener("change",setvolume,false);
-	fullscreenbtn.addEventListener("click",toggleFullScreen,false);
+let player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("yt-player", {
+    width: "660",
+    height: "405",
+    videoId: "zmg_jOwa9Fc",
+    playerVars: {
+      controls: 0,
+      disablekb: 0,
+      showinfo: 0,
+      rel: 0,
+      autoplay: 0,
+      modestbranding: 0
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    }
+  });
 }
-window.onload = intializePlayer;
-function playPause(){
-	if(vid.paused){
-		vid.play();
-		playbtn.style.background = "url(pause.png)";
+
+function onPlayerReady(event) {
+  const duration = player.getDuration();
+  let interval;
+  updateTimerDisplay();
+
+  $(".player").removeClass("hidden");
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    const completed = player.getCurrentTime();
+    const percents = (completed / duration) * 100;
+
+    changeButtonPosition(percents);
+
+    updateTimerDisplay();
+  }, 1000);
+}
+
+function onPlayerStateChange(event) {
+  const playerButton = $(".player__start");
+  switch (event.data) {
+    case 1:
+      $(".player__wrapper").addClass("active");
+      playerButton.addClass("paused");
+      break;
+    case 2: 
+      playerButton.removeClass("paused");
+      break;
+  }
+}
+
+$(".player__start").on("click", e => {
+  const playerStatus = player.getPlayerState(); // 0 - ended, 1 - played, 2 - paused ...
+
+  if (playerStatus !== 1) {
+    player.playVideo();
+  } else {
+    player.pauseVideo();
+  }
+});
+
+
+$(".player__playback").on("click", e => {
+  e.preventDefault();
+  const bar = $(e.currentTarget);
+  const newButtonPosition = e.pageX - bar.offset().left;
+  const clickedPercents = (newButtonPosition / bar.width()) * 100;
+  const newPlayerTime = (player.getDuration() / 100) * clickedPercents;
+
+  changeButtonPosition(clickedPercents);
+  player.seekTo(newPlayerTime);
+});
+
+$(".player__splash").on("click", e => {
+  player.playVideo();
+});
+
+
+
+function changeButtonPosition(percents) {
+  $(".player__playback-button").css({
+    left: `${percents}%`
+  });
+}
+
+function updateTimerDisplay() {
+  $(".player__duration-completed").text(formatTime(player.getCurrentTime()));
+  $(".player__duration-estimate").text(formatTime(player.getDuration()));
+}
+
+function formatTime(time) {
+  const roundTime = Math.round(time);
+
+  const minutes = Math.floor(roundTime / 60);
+  const seconds = roundTime - minutes * 60;
+  const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return minutes + ":" + formatedSeconds;
+}
+
+
+
+
+
+$(".player__volum").on("click", e => {
+	e.preventDefault();
+	const bar = $(e.currentTarget);
+	const newButton = e.pageX - bar.offset().left;
+	const clickedPercents = (newButton / bar.width()) * 100;
+	const newPlayerTime = (player.getVolume() / 100) * clickedPercents;
+  
+	changeButton(clickedPercents);
+	player.setVolume(newPlayerTime);
+	console.log(clickedPercents);
+  });
+  
+ 
+  
+  
+  
+  function changeButton(percents) {
+	$(".player__volum-button").css({
+	  left: `${percents}%`
+	});
+  }
+
+
+  $('.player__volume').on('click', e => {
+	if (!player.isMuted()) {
+		player.mute()
 	} else {
-		vid.pause();
-		playbtn.style.background = "url(play.png)";
+		player.unMute()
 	}
-}
-function vidSeek(){
-	var seekto = vid.duration * (seekslider.value / 100);
-	vid.currentTime = seekto;
-}
-function seektimeupdate(){
-	var nt = vid.currentTime * (100 / vid.duration);
-	seekslider.value = nt;
-	var curmins = Math.floor(vid.currentTime / 60);
-	var cursecs = Math.floor(vid.currentTime - curmins * 60);
-	var durmins = Math.floor(vid.duration / 60);
-	var dursecs = Math.floor(vid.duration - durmins * 60);
-	if(cursecs < 10){ cursecs = "0"+cursecs; }
-	if(dursecs < 10){ dursecs = "0"+dursecs; }
-	if(curmins < 10){ curmins = "0"+curmins; }
-	if(durmins < 10){ durmins = "0"+durmins; }
-	curtimetext.innerHTML = curmins+":"+cursecs;
-	durtimetext.innerHTML = durmins+":"+dursecs;
-}
-function vidmute(){
-	if(vid.muted){
-		vid.muted = false;
-		mutebtn.innerHTML = "Mute";
-	} else {
-		vid.muted = true;
-		mutebtn.innerHTML = "Unmute";
-	}
-}
-function setvolume(){
-	vid.volume = volumeslider.value / 100;
-}
-function toggleFullScreen(){
-	if(vid.requestFullScreen){
-		vid.requestFullScreen();
-	} else if(vid.webkitRequestFullScreen){
-		vid.webkitRequestFullScreen();
-	} else if(vid.mozRequestFullScreen){
-		vid.mozRequestFullScreen();
-	}
-}
+})
